2737811fa45fbc66fbe3a36a068b21cb35a49dc0
cvalleitu <c.valleitu@gmail.com>
2025-03-22 13:37:13 +0100

    mod 3

---
+++ Mod 3/jupyters clase/src/soporte.py
@@ -0,0 +1,21 @@
+import pandas as pd
+
+def exploracion(dataframe):
+    """Función que recibe un dataframe y bli bli bla bla
+    RETURN una variable que bli bli bla bla"""
+
+    display(dataframe.head())
+
+
+nombre = "Cesar"
+
+otro = "Valle"
+
+
+
+
+
+
+
+
+
